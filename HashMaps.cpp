c0d0a6022a993d2935f32fc401a7d99aa09f4d12
Rohit Kumar <rohitdhankhar7347@gmail.com>
2026-01-28 19:07:36 +0530

    added logic behind O(1) searching behind UnorderedMap

--- HashMaps.cpp
+++ HashMaps.cpp
@@ -15,6 +15,20 @@
     - Using Array
 
 */
+
+/*
+How O(1) searching in unordered Using array implementation:
+    - There is an Hash function that converts given key(Any datatype) to an integer
+    - Hash function = Hash Code + Compression Function
+        - Hash code : The formula that is being used to convert key to an integer
+        - Compression Function: It bring to generated int inside the bounds of array
+    - Now using the inbounded index to store the value in array
+    - When searching is done the key provided again go through Hashing and result at generated index is returned.
+    - Hash Code should be as such that there are minimum collision for different keys.
+    - Collisiong Handling
+        - Open Hashing:Create a linked list for the keys where collision is occuring
+        - Closed Adressing:If collision occur find out next free space and insert there.
+*/
 #include<iostream>
 #include<unordered_map>
 using namespace std;
