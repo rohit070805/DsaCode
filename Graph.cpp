1b299b98acd18b51bdc255d55877c1665fb72a64
Rohit Kumar <rohitdhankhar7347@gmail.com>
2026-02-13 17:20:14 +0530

    added list implementation of graph

--- Graph.cpp
+++ Graph.cpp
@@ -1,3 +1,5 @@
+#include <bits/stdc++.h>
+using namespace std;
 /*
 Graph:
     - Combination of edges and nodes
@@ -29,4 +31,38 @@ Implementation:
         - arr[i][j] = 0{if there is no edge between i and jth node}
 
     - Adjancy List
+        - har ek node ke liye ek list bnao {here in this node store the neighbours for this node}
 */
+
+class Graph{
+    public:
+    unordered_map<int,list<int>> adjList;
+    void addEdge(int u,int v,bool directed){
+        if(directed){
+            adjList[u].push_back(v);
+        }else{
+            adjList[u].push_back(v);
+            adjList[v].push_back(u);
+        }
+        cout<<"Printting list"<<endl;
+        printAdjList();
+        cout<<endl;
+    }
+    void printAdjList(){
+        for(auto i:adjList){
+            cout<<i.first <<" ->{";
+            for(auto j:i.second){
+                cout<<j<<",";
+            }
+            cout<<"}"<<endl;
+        }
+    }
+};
+int main(){
+    Graph g;
+    g.addEdge(0,1,1);
+     g.addEdge(1,2,1);
+      g.addEdge(1,3,1);
+       g.addEdge(3,1,1);
+       return 0;
+}
